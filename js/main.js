3e286733f4624d9be2625347854536b45fd4c704
mechno <mechnotechno@gmail.com>
2019-07-11 20:45:54 +0900

    ファイル差分コピー

---
+++ js/main.js
@@ -0,0 +1,90 @@
+// トップスライダー1
+var mySwiper = new Swiper( '.swiper-main', {
+  loop: true,
+  virtualTranslate: false,
+  roundLengths: false,
+  autoplay: {
+    delay: 5000
+  },
+  speed: 2000
+
+  // effect: "fade"
+});
+
+// トップスライダー2
+var mySwiper = new Swiper( '.swiper-reform ', {
+  navigation: {
+    nextEl: '.swiper-reform-next',
+    prevEl: '.swiper-reform-prev'
+  },
+  pagination: {
+    el: '.swiper-reform-page',
+    type: 'bullets',
+    clickable: true
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 5.5,
+  spaceBetween: 30,
+  initialSlide: 0,
+  centeredSlides: true,
+  loop: true,
+  speed: 1500,
+  breakpoints: {
+    767: {
+      slidesPerView: 4
+    }
+  }
+});
+
+// トップスライダー3
+var mySwiper = new Swiper( '.swiper-exterior', {
+  navigation: {
+    nextEl: '.swiper-exterior-next',
+    prevEl: '.swiper-exterior-prev'
+  },
+  pagination: {
+    el: '.swiper-exterior-page',
+    type: 'bullets',
+    clickable: true
+  },
+
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false
+  // },
+  slidesPerView: 5.5,
+  spaceBetween: 30,
+  initialSlide: 0,
+  centeredSlides: true,
+  loop: true,
+  speed: 1500
+});
+
+// ページトップへ戻る
+$( function() {
+  var TopBtn = $( '#pageTop' );
+  TopBtn.hide();
+
+  // スクロール位置が100でボタンを表示
+  $( window ).scroll( function() {
+    if ( 500 < $( this ).scrollTop() ) {
+      TopBtn.fadeIn();
+    } else {
+      TopBtn.fadeOut();
+    }
+  });
+
+  // ボタンを押下するとトップへ移動
+  TopBtn.click( function() {
+    $( 'body,html' ).animate(
+      {
+        scrollTop: 0
+      },
+      'slow'
+    );
+    return false;
+  });
+});
