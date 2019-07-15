a13269c025fc9dfddd83b4b2f23cd88fff27defa
ichi-com <y.ishiguro@ichi-com.co.jp>
2019-07-16 08:48:45 +0900

    会社連休明けコミット

---
+++ js/page-top.js
@@ -0,0 +1,25 @@
+// ページトップへ戻る
+$( function() {
+  var TopBtn = $( '#pageTop' );
+  TopBtn.hide();
+
+  // スクロール位置が100でボタンを表示
+  $( window ).scroll( function() {
+    if ( 500 < $( this ).scrollTop() ) {
+      TopBtn.fadeIn();
+    } else {
+      TopBtn.fadeOut();
+    }
+  });
+
+  // ボタンを押下するとトップへ移動
+  TopBtn.click( function() {
+    $( 'body,html' ).animate(
+      {
+        scrollTop: 0
+      },
+      'slow'
+    );
+    return false;
+  });
+});
