cd0adb2e05fdb61d8d2fa6136b116e7c9a2bde56
ichi-com <y.ishiguro@ichi-com.co.jp>
2019-07-24 16:50:05 +0900

    モーダル　トップリンク

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 // objectFit
 objectFitImages( 'img.of-width', 'img.of-height' );
 
+// モーダル
+$( '.modal' ).modaal({
+  type: 'inline', // コンテンツのタイプを指定
+  animation_speed: '500', // アニメーションのスピードをミリ秒単位で指定
+  background: '#333', // 背景の色を白に変更
+  overlay_opacity: '0.6', // 背景のオーバーレイの透明度を変更
+  fullscreen: 'false', // フルスクリーンモードにする
+  background_scroll: 'true', // 背景をスクロールさせるか否か
+  loading_content: 'Now Loading, Please Wait.', // 読み込み時のテキスト表示
+  width: '500',
+  height: '500'
+});
+
 // ページトップへ戻る
 $( function() {
   var TopBtn = $( '#pageTop' );
